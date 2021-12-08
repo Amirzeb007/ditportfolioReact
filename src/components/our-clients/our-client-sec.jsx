5c4d9707307ce8d8dddaf4f967d6346101fe56ad
Amirzeb007 <discoveramirzeb@gmail.com>
2021-12-08 12:44:27 +0500

    Sec Commit

--- src/components/our-clients/our-client-sec.jsx
+++ src/components/our-clients/our-client-sec.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// import ReactOwlCarousel from 'react-owl-carousel';
-// import "owl.carousel/dist/assets/owl.carousel.css";
-// import "owl.carousel/dist/assets/owl.theme.default.css";
 import MyOwlCarousel from '../owlcaro/owl-carousal';
 
 function OurClient(props) {
